1255f96ed1685773bd70ec259d76c9007b026bce
Winston Chan <winstonchan05@gmail.com>
2022-06-22 16:57:24 -0700

    dynamic changing password

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
+import { useState } from "react";
+import React from "react";
+
 function Generator() {
-  const generate = function (length) {
-    let password = "";
+  let [password, setPassword] = useState("AREA FOR PASSWORD");
+  const generate = () => {
+    password = "";
     const SYMBOLS =
       "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789~`!@#$%^&*()_-+={[}]|:;\"'<,>.?/";
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < 16; i++) {
       password += SYMBOLS.charAt(Math.floor(Math.random() * SYMBOLS.length));
     }
-    console.log(password);
+    setPassword(password);
   };
 
-  return <button onClick={generate}>Generate Password</button>;
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <button
+        onClick={generate}
+        className='text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700'
+      >
+        Generate Password
+      </button>
+      <h1>{password}</h1>
+    </div>
+  );
 }
 
 function App() {
   return (
-    <div className='App'>
+    <div>
       <Generator />
     </div>
   );
